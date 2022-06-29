89b5ca4b45e54d0ecb05b197c6967849d6a272fa
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2022-06-28 21:46:26 -0300

    chore: edit config of docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://tresdoce.github.io/tresdoce-nestjs-toolkit-docs/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
