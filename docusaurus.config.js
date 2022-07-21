9660aa8e2052d615f90e80b6a1ef5c79175b9b1a
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2022-07-20 22:22:38 -0300

    chore: add baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,134 +6,133 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: "Portal de documentación",
-    tagline:
-        "Acá vas a encontrar recursos para empezar a desarrollar tus aplicaciones",
-    url: 'https://tresdoce.github.io/tresdoce-nestjs-toolkit-docs/',
-    baseUrl: '/',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
-    organizationName: 'tresdoce', // Usually your GitHub org/user name.
-    projectName: 'tresdoce-nestjs-docs', // Usually your repo name.
-    i18n: {
-        defaultLocale: 'es',
-        locales: ['es'],
-    },
-    themeConfig:
+  title: 'Portal de documentación',
+  tagline: 'Acá vas a encontrar recursos para empezar a desarrollar tus aplicaciones',
+  url: 'https://tresdoce.github.io/tresdoce-nestjs-toolkit-docs/',
+  baseUrl: '/tresdoce-nestjs-toolkit-docs/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'tresdoce', // Usually your GitHub org/user name.
+  projectName: 'tresdoce-nestjs-docs', // Usually your repo name.
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es'],
+  },
+  themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
-            navbar: {
-                title: 'NestJSPedia',
-                logo: {
-                    alt: 'Logo tresdoce',
-                    src: 'img/logo.svg',
-                },
-                items: [
-                    {
-                        type: 'doc',
-                        docId: 'intro',
-                        position: 'left',
-                        label: 'Tutorial',
-                    },
-                    {
-                        to: '/blog',
-                        label: 'Blog',
-                        position: 'left'
-                    },
-                    {
-                        href: 'https://github.com/tresdoce/tresdoce-nestjs-toolkit',
-                        label: 'GitHub',
-                        position: 'right',
-                    },
-                ],
-            },
-            footer: {
-                style: 'dark',
-                links: [
-                    {
-                        title: 'Docs',
-                        items: [
-                            {
-                                label: 'Tutorial',
-                                to: '/docs/intro',
-                            },
-                        ],
-                    },
-                    {
-                        title: 'Community',
-                        items: [
-                            {
-                                label: 'Stack Overflow',
-                                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-                            },
-                            {
-                                label: 'Discord',
-                                href: 'https://discordapp.com/invite/docusaurus',
-                            },
-                            {
-                                label: 'Twitter',
-                                href: 'https://twitter.com/docusaurus',
-                            },
-                        ],
-                    },
-                    {
-                        title: 'More',
-                        items: [
-                            {
-                                label: 'Blog',
-                                to: '/blog',
-                            },
-                            {
-                                label: 'GitHub',
-                                href: 'https://github.com/facebook/docusaurus',
-                            },
-                        ],
-                    },
-                ],
-                copyright: `Copyright © ${new Date().getFullYear()}. Built with ♥️.`,
-            },
-            prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
-            },
-            colorMode: {
-                defaultMode: 'dark',
-                disableSwitch: false,
-                respectPrefersColorScheme: true,
-            }
-        }),
-    presets: [
-        [
-            'classic',
-            ({
-                docs: {
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    remarkPlugins: [require("mdx-mermaid")],
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    //editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                },
-                blog: {
-                    showReadingTime: true,
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    //editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
-                },
-            }),
+    ({
+      navbar: {
+        title: 'NestJSPedia',
+        logo: {
+          alt: 'Logo tresdoce',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {
+            to: '/blog',
+            label: 'Blog',
+            position: 'left',
+          },
+          {
+            href: 'https://github.com/tresdoce/tresdoce-nestjs-toolkit',
+            label: 'GitHub',
+            position: 'right',
+          },
         ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()}. Built with ♥️.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
+    }),
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          remarkPlugins: [require('mdx-mermaid')],
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          //editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          //editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
     ],
-    plugins: [
-        [
-            require.resolve("docusaurus-lunr-search"),
-            {
-                languages: ["es"], // language codes
-            },
-        ]
+  ],
+  plugins: [
+    [
+      require.resolve('docusaurus-lunr-search'),
+      {
+        languages: ['es'], // language codes
+      },
     ],
+  ],
 };
 
 module.exports = config;
--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -1,44 +1,44 @@
-import React, { useRef, useCallback, useState } from "react";
-import classnames from "classnames";
-import { useHistory } from "@docusaurus/router";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import React, { useRef, useCallback, useState } from 'react';
+import classnames from 'classnames';
+import { useHistory } from '@docusaurus/router';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { usePluginData } from '@docusaurus/useGlobalData';
-const Search = props => {
+const Search = (props) => {
   const initialized = useRef(false);
   const searchBarRef = useRef(null);
   const [indexReady, setIndexReady] = useState(false);
   const history = useHistory();
-  const { siteConfig = {}, isClient = false} = useDocusaurusContext();
+  const { siteConfig = {}, isClient = false } = useDocusaurusContext();
   const { baseUrl } = siteConfig;
   const initAlgolia = (searchDocs, searchIndex, DocSearch) => {
-      new DocSearch({
-        searchDocs,
-        searchIndex,
-        inputSelector: "#search_input_react",
-        // Override algolia's default selection event, allowing us to do client-side
-        // navigation and avoiding a full page refresh.
-        handleSelected: (_input, _event, suggestion) => {
-          const url = baseUrl + suggestion.url;
-          // Use an anchor tag to parse the absolute url into a relative url
-          // Alternatively, we can use new URL(suggestion.url) but its not supported in IE
-          const a = document.createElement("a");
-          a.href = url;
-          // Algolia use closest parent element id #__docusaurus when a h1 page title does not have an id
-          // So, we can safely remove it. See https://github.com/facebook/docusaurus/issues/1828 for more details.
+    new DocSearch({
+      searchDocs,
+      searchIndex,
+      inputSelector: '#search_input_react',
+      // Override algolia's default selection event, allowing us to do client-side
+      // navigation and avoiding a full page refresh.
+      handleSelected: (_input, _event, suggestion) => {
+        const url = baseUrl + suggestion.url;
+        // Use an anchor tag to parse the absolute url into a relative url
+        // Alternatively, we can use new URL(suggestion.url) but its not supported in IE
+        const a = document.createElement('a');
+        a.href = url;
+        // Algolia use closest parent element id #__docusaurus when a h1 page title does not have an id
+        // So, we can safely remove it. See https://github.com/facebook/docusaurus/issues/1828 for more details.
 
-          history.push(url);
-        }
-      });
+        history.push(url);
+      },
+    });
   };
 
   const pluginData = usePluginData('docusaurus-lunr-search');
   const getSearchDoc = () =>
-    process.env.NODE_ENV === "production"
+    process.env.NODE_ENV === 'production'
       ? fetch(`${baseUrl}${pluginData.fileNames.searchDoc}`).then((content) => content.json())
       : Promise.resolve([]);
 
   const getLunrIndex = () =>
-    process.env.NODE_ENV === "production"
+    process.env.NODE_ENV === 'production'
       ? fetch(`${baseUrl}${pluginData.fileNames.lunrIndex}`).then((content) => content.json())
       : Promise.resolve([]);
 
@@ -47,10 +47,10 @@ const Search = props => {
       Promise.all([
         getSearchDoc(),
         getLunrIndex(),
-        import("./lib/DocSearch"),
-        import("./algolia.css")
+        import('./lib/DocSearch'),
+        import('./algolia.css'),
       ]).then(([searchDocs, searchIndex, { default: DocSearch }]) => {
-        if( searchDocs.length === 0) {
+        if (searchDocs.length === 0) {
           return;
         }
         initAlgolia(searchDocs, searchIndex, DocSearch);
@@ -61,14 +61,14 @@ const Search = props => {
   };
 
   const toggleSearchIconClick = useCallback(
-    e => {
+    (e) => {
       if (!searchBarRef.current.contains(e.target)) {
         searchBarRef.current.focus();
       }
 
       props.handleSearchBarToggle && props.handleSearchBarToggle(!props.isSearchBarExpanded);
     },
-    [props.isSearchBarExpanded]
+    [props.isSearchBarExpanded],
   );
 
   if (isClient) {
@@ -80,8 +80,8 @@ const Search = props => {
       <span
         aria-label="expand searchbar"
         role="button"
-        className={classnames("search-icon", {
-          "search-icon-hidden": props.isSearchBarExpanded
+        className={classnames('search-icon', {
+          'search-icon-hidden': props.isSearchBarExpanded,
         })}
         onClick={toggleSearchIconClick}
         onKeyDown={toggleSearchIconClick}
@@ -93,9 +93,9 @@ const Search = props => {
         placeholder={indexReady ? 'Search' : 'Loading...'}
         aria-label="Search"
         className={classnames(
-          "navbar__search-input",
-          { "search-bar-expanded": props.isSearchBarExpanded },
-          { "search-bar": !props.isSearchBarExpanded }
+          'navbar__search-input',
+          { 'search-bar-expanded': props.isSearchBarExpanded },
+          { 'search-bar': !props.isSearchBarExpanded },
         )}
         onClick={loadAlgolia}
         onMouseOver={loadAlgolia}
